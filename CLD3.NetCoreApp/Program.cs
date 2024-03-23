aac9e7c17e08e4883a1bddec9974984789550d45
Evgeny Nikulov <evgeny.nikulov@innostage-group.ru>
2024-03-23 08:55:07 +0300

    added .net core 6.0 dll + .net core 6.0 test concept app

---
+++ CLD3.NetCoreApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CLD3NetCore;
+
+namespace LanguageDetectionApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var detector = new LanguageDetectorLibrary();
+            var Rulanguage=detector.DetectLanguages("Привет, как дела?", 3);
+            var Enlanguage=detector.DetectLanguages("How are you?", 3);
+        }
+
+    }
+
+    public class Prediction
+    {
+        public string language { get; set; }
+        public float probability { get; set; }
+        public int is_reliable { get; set; }
+        public float proportion { get; set; }
+    }
+}
---
+++ CLD3.NetCoreLib/LanguageDetector.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using CLD3Net;
+
+namespace CLD3NetCore
+{
+    public class LanguageDetectorLibrary
+    {
+        private LanguageDetector detector;
+
+        public LanguageDetectorLibrary()
+        {
+            detector = new LanguageDetector();
+        }
+
+        public List<Prediction> DetectLanguages(string text, int numberOfLanguages)
+        {
+            string? predictions = detector.DetectNMostFreqLangs(text, numberOfLanguages);
+            var Predictions = JsonConvert.DeserializeObject<List<Prediction>>(predictions);
+            return Predictions;
+        }
+    }
+
+    public class Prediction
+    {
+        public string language { get; set; }
+        public float probability { get; set; }
+        public int is_reliable { get; set; }
+        public float proportion { get; set; }
+    }
+}
